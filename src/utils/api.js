1b2412b1dca993c13c7a9675d2368c3dc0322458
rodiontazetdinov <rodiontazetdinov@yadnex.ru>
2023-07-12 15:57:37 +0300

    feat: смена ip

--- src/utils/api.js
+++ src/utils/api.js
@@ -97,7 +97,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "http://34.92.175.180:8000",
+  baseUrl: "http://34.150.15.242:8000",
   headers: {
     "Content-Type": "application/json",
   },
